6aab1f4caf89a941d675536ccd48be02d26e5ba9
justinnamilee <justin.nami.lee@gmail.com>
2023-01-30 17:14:22 -0400

    starting on data import and validation

---
+++ public/script/game/validateData.js
@@ -0,0 +1,3 @@
+import {ja, en} from "/script/japanese.js"
+
+console.log(ja);
---
+++ public/script/japanese.js
@@ -0,0 +1,23 @@
+export const ja = {
+  hiragana: "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわを".split(),
+  hiraganaDiacritic: "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ".split(),
+  hiraganaDigraph: "きゃ きゅ きょ しゃ しゅ しょ ちゃ ちゅ ちょ にゃ にゅ にょ ひゃ ひゅ ひょ みゃ みゅ みょ りゃ りゅ りょ ぎゃ ぎゅ ぎょ じゃ じゅ じょ ぢゃ ぢゅ ぢょ びゃ びゅ びょ ぴゃ ぴゅ ぴょ".split(" "),
+  katakana: "".split(),
+  katakanaDiacritic: "".split(),
+  katakanaDigraph: "".split(" "),
+  katakanaExtended: "".split(" ")
+};
+
+export const en = {
+  hiragana: "a i u e o ka ki ku ke ko sa shi su se so ta chi tsu te to na ni nu ne no ha hi fu he ho ma mi mu me mo ya yu yo ra ri ru re ro wa wo".split(" "),
+  hiraganaDiacritic: "ga gi gu ge go za ji zu ze zo da ji ze de do ba bi bu be bo pa pi pu pe po".split(" "),
+  hiraganaDigraph: "kya kyu kyo sha shu sho cha chu cho nya nyu nyo hya hyu hyo mya myu myo rya ryu ryo".split(" "),
+  katakana: "".split(" "),
+  katakanaDiacritic: "".split(" "),
+  katakanaDigraph: "".split(" "),
+  katakanaExtended: "".split(" ")
+};
+
+export function validateKana() {
+  return 1;
+}
