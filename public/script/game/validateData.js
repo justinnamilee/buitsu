8ba4d87fd8552fdd8cc8d62bec642e8db802964e
justinnamilee <justin.nami.lee@gmail.com>
2023-01-30 21:07:25 -0400

    basic, diacritical, and digraph sets in

--- public/script/game/validateData.js
+++ public/script/game/validateData.js
@@ -1,3 +1,24 @@
-import {ja, en} from "/script/japanese.js"
+import {hiragana, katakana, romaji} from "/script/japanese.js"
 
-console.log(ja);
+console.log("Hiragana object:");
+console.log(hiragana);
+
+console.log("");
+console.log("Katakana object:");
+console.log(katakana);
+
+console.log("");
+console.log("Romaji object:");
+console.log(romaji);
+
+console.log("");
+console.log("Hiragana checks:");
+for (const l in hiragana) {
+  console.log(l + " is valid: " + String(hiragana[l].length === romaji[l].length))
+}
+
+console.log("");
+console.log("Katakana checks:");
+for (const l in katakana) {
+  console.log(l + " is valid: " + String(katakana[l].length === romaji[l].length))
+}
--- public/script/japanese.js
+++ public/script/japanese.js
@@ -1,23 +1,20 @@
-export const ja = {
-  hiragana: "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわを".split(),
-  hiraganaDiacritic: "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ".split(),
-  hiraganaDigraph: "きゃ きゅ きょ しゃ しゅ しょ ちゃ ちゅ ちょ にゃ にゅ にょ ひゃ ひゅ ひょ みゃ みゅ みょ りゃ りゅ りょ ぎゃ ぎゅ ぎょ じゃ じゅ じょ ぢゃ ぢゅ ぢょ びゃ びゅ びょ ぴゃ ぴゅ ぴょ".split(" "),
-  katakana: "".split(),
-  katakanaDiacritic: "".split(),
-  katakanaDigraph: "".split(" "),
-  katakanaExtended: "".split(" ")
+export const hiragana = {
+  basic: "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわを".split(""),
+  diacritic: "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ".split(""),
+  digraph: "きゃ きゅ きょ しゃ しゅ しょ ちゃ ちゅ ちょ にゃ にゅ にょ ひゃ ひゅ ひょ みゃ みゅ みょ りゃ りゅ りょ ぎゃ ぎゅ ぎょ じゃ じゅ じょ ぢゃ ぢゅ ぢょ びゃ びゅ びょ ぴゃ ぴゅ ぴょ".split(" ")
 };
 
-export const en = {
-  hiragana: "a i u e o ka ki ku ke ko sa shi su se so ta chi tsu te to na ni nu ne no ha hi fu he ho ma mi mu me mo ya yu yo ra ri ru re ro wa wo".split(" "),
-  hiraganaDiacritic: "ga gi gu ge go za ji zu ze zo da ji ze de do ba bi bu be bo pa pi pu pe po".split(" "),
-  hiraganaDigraph: "kya kyu kyo sha shu sho cha chu cho nya nyu nyo hya hyu hyo mya myu myo rya ryu ryo".split(" "),
-  katakana: "".split(" "),
-  katakanaDiacritic: "".split(" "),
-  katakanaDigraph: "".split(" "),
-  katakanaExtended: "".split(" ")
+export const katakana = {
+  basic: "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲ".split(""),
+  diacritic: "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ".split(""),
+  digraph: "キャ キュ キョ シャ シュ ショ チャ チュ チョ ニャ ニュ ニョ ヒャ ヒュ ヒョ ミャ ミュ ミョ リャ リュ リョ ギャ ギュ ギョ ジャ ジュ ジョ ヂャ ヂュ ヂョ ビャ ビュ ビョ ピャ ピュ ピョ".split(" "),
+  extended: "".split(" ")
 };
 
-export function validateKana() {
-  return 1;
-}
+export const romaji = {
+  basic: "a i u e o ka ki ku ke ko sa shi su se so ta chi tsu te to na ni nu ne no ha hi fu he ho ma mi mu me mo ya yu yo ra ri ru re ro wa wo".split(" "),
+  diacritic: "ga gi gu ge go za ji zu ze zo da ji ze de do ba bi bu be bo pa pi pu pe po".split(" "),
+  digraph: "kya kyu kyo sha shu sho cha chu cho nya nyu nyo hya hyu hyo mya myu myo rya ryu ryo gya gyu gyo ja ju jo ja ju jo bya byu byo pya pyu pyo".split(" "),
+  // only for katakana
+  extended: "".split(" ")
+};
